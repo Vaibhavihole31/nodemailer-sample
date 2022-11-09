8c846dcc8f28c380a08ab465e14e7acc30a106e0
Vaibhavihole31 <itspinki05@gmail.com>
2022-11-09 15:10:05 +0530

    feat: meassage card

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './Message.css'
+import './Message.css';
+import headerImg from './gmail.png';
+import swal from "sweetalert";
 
 function Message() {
 
@@ -15,64 +17,58 @@ function Message() {
       text: text
     })
 
-    if (response.data.success) {
-      alert("Message Send Successfully !!");
-      
-    }
-    else {
-      alert("Invalid Credentials")
-    }
+    setTo("");
+    setSubject("");
+    setText("");
 
-      setTo("");
-      setSubject("");
-      setText("");
+    swal("Message Send Successfully !!");
   }
 
   return (
     <>
       <div className='container mt-5'>
-      <div className=' message-card '>
-        <div className='row'>
-          <div className='col-md-6'>
+        <div className=' message-card '>
+          <div className='row'>
+            <div className='col-md-6'>
+              <h4><img className='gmail-img' src={headerImg} /> Gmail</h4>
+            </div>
+            <div className='col-md-4 send-message-card  mt-5 mb-3'>
+              <form>
+                <h4 className='text-center mt-5 mb-5'>New Message</h4>
+                <div className="mb-3">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    placeholder="Email"
+                    value={to} onChange={(e) => { setTo(e.target.value) }}
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="subject"
+                    placeholder="subject"
+                    value={subject} onChange={(e) => { setSubject(e.target.value) }}
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="subject"
+                    placeholder="text"
+                    value={text} onChange={(e) => { setText(e.target.value) }}
+                  />
+                </div>
+                <button className="login-page-btn btn btn-primary w-100 mb-3" type="button" onClick={send}>
+                  <i class="fa-solid fa-right-to-bracket"></i> Send
+                </button>
+              </form>
+            </div>
 
           </div>
-          <div className='col-md-4 message-card  mt-5 mb-3'>
-            <form>
-            <h4 className='text-center mt-5 mb-5'>New Message</h4>
-              <div className="mb-3">
-                <input
-                  type="email"
-                  className="form-control"
-                  id="email"
-                  placeholder="Email"
-                  value={to} onChange={(e) => { setTo(e.target.value) }}
-                />
-              </div>
-              <div className="mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="subject"
-                  placeholder="text"
-                  value={subject} onChange={(e) => { setSubject(e.target.value) }}
-                />
-              </div>
-              <div className="mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="subject"
-                  placeholder="text"
-                  value={text} onChange={(e) => { setText(e.target.value) }}
-                />
-              </div>
-              <button className="login-page-btn btn btn-primary w-100 mb-3" type="button" onClick={send}>
-                <i class="fa-solid fa-right-to-bracket"></i> Send
-              </button>
-            </form>
-          </div>
-
-        </div>
         </div>
       </div>
     </>
